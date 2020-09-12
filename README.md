98244787aef3d041517ff445c52d96fed4d1ab43
bparinas <bparinas@hotmail.com>
2020-09-12 20:00:27 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# restapi-py
+Installation:
+1. Create your python virtual environment
+$ python3 -m venv pyapi
+$ source pyapi/bin/activate
+
+2. Clone this repository
+$ git clone https://github.com/bparinas/restapi-py.git
+$ cd restapi-py
+
+2. Install latest flask and sqlite3
+$ pip install sqlite3
+$ pip install Flask
+$ flask run --host=0.0.0.0 --port=8000
+
+
+Use cases:
+
+#Get all events
+request:
+curl -i -X GET http://34.70.25.201:8000/events
+
+respond:
+{
+    "data": [{
+        "endtime": "12:00",
+        "event_location": "Tokyo",
+        "event_name": "Birthday",
+        "id": 1,
+        "rsvp_email": "user1@example.com",
+        "starttime": "10:00"
+    }, {
+        "endtime": "21:00",
+        "event_location": "Osaka",
+        "event_name": "Reunion",
+        "id": 2,
+        "rsvp_email": "user2@example.com",
+        "starttime": "15:00"
+    }]
+}
+
+#Get specific event details
+request:
+curl -i -X GET http://34.70.25.201:8000/events/1
+
+respond:
+{
+    "endtime": "12:00",
+    "event_location": "Tokyo",
+    "event_name": "Birthday",
+    "id": 1,
+    "rsvp_email": "user1@example.com",
+    "starttime": "10:00"
+}
+
+
+#Add/sign-up event
+request:
+curl -i -X POST http://34.70.25.201:8000/events -H "Content-Type: application/json" -d '{"event_name":"Conferences", "event_location":"Kyoto", "starttime":"09:00", "endtime":"17:00", "rsvp_email":"user3@example.com"}'
+
+respond:
+{
+    "endtime": "17:00",
+    "event_location": "Kyoto",
+    "event_name": "Conferences",
+    "id": 3,
+    "rsvp_email": "user3@example.com",
+    "starttime": "09:00"
+}
+
+
+#Update event details
+request:
+curl -i -X PUT http://34.70.25.201:8000/events/3 -H "Content-Type: application/json" -d '{"event_name":"Conferences", "event_location":"Kyoto", "starttime":"09:00", "endtime":"17:00", "rsvp_email":"user4@example.com"}'
+
+respond:
+{
+    "endtime": "17:00",
+    "event_location": "Kyoto",
+    "event_name": "Conferences",
+    "id": 3,
+    "rsvp_email": "user4@example.com",
+    "starttime": "09:00"
+}
+
+verify update:
+curl -i -X GET http://34.70.25.201:8000/events/3
+
+respond:
+{
+    "endtime": "17:00",
+    "event_location": "Kyoto",
+    "event_name": "Conferences",
+    "id": 3,
+    "rsvp_email": "user4@example.com",
+    "starttime": "09:00"
+}
+
+
+#Delete event 3
+request:
+curl -i -X DELETE http://34.70.25.201:8000/events/3
+
+respond:
+HTTP/1.0 204 NO CONTENT
+Content-Type: text/html; charset=utf-8
+
+verify update (expected no event #3):
+curl -i -X GET http://34.70.25.201:8000/events
---
+++ restapi.py
@@ -0,0 +1,108 @@
+from flask import Flask, jsonify, request
+
+from http import HTTPStatus
+
+app = Flask(__name__)
+
+events = [
+    {
+        'id': 1,
+        'event_name': 'Birthday',
+        'event_location': 'Tokyo',
+        'starttime': '10:00',
+        'endtime': '12:00',
+        'rsvp_email': 'user1@example.com'
+    },
+    {
+        'id': 2,
+        'event_name': 'Reunion',
+        'event_location': 'Osaka',
+        'starttime': '15:00',
+        'endtime': '21:00',
+        'rsvp_email': 'user2@example.com'
+    }
+]
+
+@app.route('/events/', methods=['GET'])
+
+#list events
+def get_events():
+
+    return jsonify({'data': events})
+
+@app.route('/events/<int:event_id>', methods=['GET'])
+
+#list specific event
+def get_event(event_id):
+
+    event = next((event for event in events if event['id'] == event_id), None)
+
+    if event:
+        return jsonify(event)
+        
+    return jsonify({'message': 'event not found'}), HTTPStatus.NOT_FOUND
+
+@app.route('/events', methods=['POST'])
+
+#sign-up event
+def signup_event():
+
+    data = request.get_json()
+
+    name = data.get('event_name')
+    location = data.get('event_location')
+    start_time = data.get('starttime')
+    end_time = data.get('endtime')
+    email = data.get('rsvp_email')
+    
+
+    event = {
+        'id': len(events) + 1,
+        'event_name': name,
+        'event_location': location,
+        'starttime': start_time,
+        'endtime': end_time,
+        'rsvp_email': email
+    }
+
+    events.append(event)
+
+    return jsonify(event), HTTPStatus.CREATED
+
+@app.route('/events/<int:event_id>', methods=['PUT'])
+
+#update event record
+def update_event(event_id):
+
+    event = next((event for event in events if event['id'] == event_id), None)
+
+    if not event:
+        return jsonify({'message': 'event not found'}), HTTPStatus.NOT_FOUND
+        
+    data = request.get_json()
+    event.update(
+        {
+            'event_name': data.get('event_name'),
+            'event_location': data.get('event_location'),
+            'starttime': data.get('starttime'),
+            'endtime': data.get('endtime'),
+            'rsvp_email': data.get('rsvp_email')
+        }
+    )
+    return jsonify(event)
+
+@app.route('/events/<int:event_id>', methods=['DELETE'])
+def delete_event(event_id):
+
+    event = next((event for event in events if event['id'] == event_id), None)
+
+    if not event:
+        return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+
+    events.remove(event)
+
+    return '', HTTPStatus.NO_CONTENT
+
+if __name__ == '__main__':
+
+    app.run()
